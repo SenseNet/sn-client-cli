90ce46230cc8db49e1ecacefb129a191d00a0a90
Lajos Gallay <gallayl@sensenet.com>
2017-04-04 11:07:38 +0200

    feat(sn.config.js): Added default sn.config.js file to the package root (will be copied as template)

---
+++ sn.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    /**
+     * The Url for your repository, e.g.: 'localhost'
+     */
+    "RepositoryUrl": "localhost",
+
+    /**
+     * The username to be used for authentication
+     */
+    "UserName": "admin"
+}
