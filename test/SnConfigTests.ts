193a6a03a924cb827130e1ffc948b2066142626d
Lajos <gallayl@users.noreply.github.com>
2017-06-06 11:48:52 +0200

    Updated tslint version (#15)
    * Updated tslint version
    
    * Update package.json
    
    * Update SnConfigTests.ts
    
    * Update index.ts
    
    Added SnConfigTests to tests/index.ts

--- test/SnConfigTests.ts
+++ test/SnConfigTests.ts
@@ -1,5 +1,6 @@
 import * as Chai from 'chai';
 import { suite, test } from 'mocha-typescript';
+import { Config } from "sn-client-js";
 
 const expect = Chai.expect;
 
@@ -8,28 +9,28 @@ export class SnConfigTests {
 
     @test
     public 'SnConfigFieldModel Should be constructed with SnConfigBehavior.Default'() {
-        const fieldModel = new SnConfigFieldModel();
-        expect(fieldModel.Behavior).to.be.eq(SnConfigBehavior.Default);
+        const fieldModel = new Config.SnConfigFieldModel();
+        expect(fieldModel.Behavior).to.be.eq(Config.SnConfigBehavior.Default);
     }
 
     @test
     public 'SnConfigFieldModelStore Should throw error if entity isn\'t in the store '() {
-        const find = () => { SnConfigFieldModelStore.Get('exampleFieldName'); };
+        const find = () => { Config.SnConfigFieldModelStore.Get('exampleFieldName'); };
         expect(find).to.throw(Error);
     }
 
     @test
     public 'SnConfigFieldModelStore Should throw an error if you try to add a field that already exists'() {
-        const add = () => { SnConfigFieldModelStore.Add({ FieldName: 'Example', Question: 'ExampleQuestion', Behavior: SnConfigBehavior.Default }); };
+        const add = () => { Config.SnConfigFieldModelStore.Add({ FieldName: 'Example', Question: 'ExampleQuestion', Behavior: Config.SnConfigBehavior.Default }); };
         add();  // add once
         expect(add).to.throw(Error);
     }
 
     @test
     public 'GetCommandOptions should return only commands that has AllowFromCommandLine flag'() {
-        const commands = SnConfigFieldModelStore.GetCommandOptions();
+        const commands = Config.SnConfigFieldModelStore.GetCommandOptions();
         commands.forEach((command) => {
-            const isAllowed = (command.Behavior & SnConfigBehavior.AllowFromCommandLine) === SnConfigBehavior.AllowFromCommandLine;
+            const isAllowed = (command.Behavior & Config.SnConfigBehavior.AllowFromCommandLine) === Config.SnConfigBehavior.AllowFromCommandLine;
             expect(isAllowed).to.eq(true);
         });
 
--- test/index.ts
+++ test/index.ts
@@ -2,4 +2,5 @@ export * from './AskTests';
 export * from './DownloadTests';
 export * from './PathHelperTests';
 export * from './StageTests';
+export * from './SnConfigTests';
 export * from './SnConfigReaderTests';
